8e8cc5eb8afa921f13a12d12b72fd68fb073d5f8
DHVSGG <diego@qapla.gg>
2023-02-15 17:53:39 -0600

    ; added

--- src/hooks/SegmentProvider.js
+++ src/hooks/SegmentProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext } from 'react';
-import { AnalyticsBrowser } from '@segment/analytics-next'
+import { AnalyticsBrowser } from '@segment/analytics-next';
 
 const SegmentContext = createContext(null);
 
